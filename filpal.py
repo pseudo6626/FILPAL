4e142c5e598a1618e063c3ef3d6661f3b26c727c
pseudo6626 <91988331+pseudo6626@users.noreply.github.com>
2021-11-15 19:42:54 -0500

    Update filpal.py

--- filpal.py
+++ filpal.py
@@ -41,8 +41,6 @@ class FILPAL:
         cmd_FILPAL_INJECTOR_help = "Runs converted gcode files and acts as a middle man for the commands"
         self.gcode.register_command('FILPAL_UPDATER', self.cmd_FILPAL_UPDATER, desc=self.cmd_FILPAL_UPDATER_help)
         cmd_FILPAL_UPDATER_help = "Updates your fil.pal file with new parameter settings for a given plastic."
-        self.gcode.register_command('FILPAL_SWAP', self.cmd_FILPAL_SWAP, desc=self.cmd_FILPAL_SWAP_help)
-        cmd_FILPAL_SWAP_help = "Used to assist in filament swaping"
 
     def cmd_FILPAL_CALIBRATE(self, gcmd):  #this command is used by filpal to run calibration tests for the different filament parameters it can regulate
         test = gcmd.get('TEST')
@@ -81,53 +79,38 @@ class FILPAL:
         un_bool=gcmd.get("UNLOAD",True)
         load_name=gcmd.get("LOAD",False)
         load_temp=gcmd.get("LOAD_TEMP",False)
-        if load_name != False:
+        un_temp=gcmd.getint("UNLOAD_TEMP",False)
+        if load_name != False and load_temp == False:
             try:
-              varfile.read(self.params_file)
-              if varfile.has_section(load_name) and load_temp !=False:
-                  load_temp=varfile.get(load_name,"hotend_temp",None)
-                  if load_temp == None and load_temp == False:
-                      msg = "New Filament does not have 'hotend_temp' defined. Please run FILPAL_LOAD or update the parameters file"
-                      logging.exception(msg)
-                      raise self.printer.command_error(msg)
+                fila=cmd_FILPAL_UPDATER(self,{"lookup":True,"FILA_ID":load_name})
+                if "hotend_temp" in fila:
+                    load_temp=int(fila["hotend_temp"])
             except:
-                 if un_temp == False:
-                    msg = "Unable to parse Loaded Filament paramters. Please run FILPAL_LOAD or update the parameters file"
-                    logging.exception(msg)
-                    raise self.printer.command_error(msg)
-
+                msg = "New Filament does not have 'hotend_temp' defined. Please run FILPAL_LOAD or update the parameters file"
+                logging.exception(msg)
+                raise self.printer.command_error(msg)
+        elif un_bool:
+            try:
+                fila=cmd_FILPAL_UPDATER(self,{"lookup":True})
+                if "hotend_temp" in fila:
+                    un_temp=int(fila["hotend_temp"])
+            except:
+                msg = "Loaded Filament does not have 'hotend_temp' defined. Please run FILPAL_LOAD or update the parameters file"
+                logging.exception(msg)
+                raise self.printer.command_error(msg)
         heater_name=gcmd.get("HEATER")
         pheaters = self.printer.lookup_object('heaters')
         try:
             heater = pheaters.lookup_heater(heater_name)
         except self.printer.config_error as e:
             raise gcmd.error(str(e))
-        un_temp=gcmd.getint("UNLOAD_TEMP",False)
-        if un_bool != False:
-            varfile = configparser.ConfigParser()
-            try:
-                varfile.read(self.params_file)
-                if varfile.has_section('Loaded'):
-                    if varfile.has_section(varfile["Loaded"]["fila_ID"]):
-                        for name, val in varfile.items('Variables'):
-                            if name == "hotend_temp":
-                                un_temp = ast.literal_eval(val)
-                            else:
-                                msg = "Loaded Filament does not have 'hotend_temp' defined. Please run FILPAL_LOAD or update the parameters file"
-                                logging.exception(msg)
-                                raise self.printer.command_error(msg)
-            except:
-                if un_temp == False:
-                    msg = "Unable to parse Loaded Filament paramters. Please run FILPAL_LOAD or update the parameters file"
-                    logging.exception(msg)
-                    raise self.printer.command_error(msg)
-            self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=%d" % (heater_name,un_temp))
-            curtemp=0
-            while curtemp < un_temp:
-                temp_call=heater.get_temp(self,self.eventtime)
-                curtemp=temp_call[0]
-            self.gcode.run_script_from_command("G1 F100 E1")
-            self.gcode.run_script_from_command("G1 F1000 E-500")
+        self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=%d" % (heater_name,un_temp))
+        curtemp=0
+        while curtemp < un_temp:
+            temp_call=heater.get_temp(self,self.eventtime)
+            curtemp=temp_call[0]
+        self.gcode.run_script_from_command("G1 F100 E1")
+        self.gcode.run_script_from_command("G1 F1000 E-500")
 
         if load_temp != False:
             self.gcode.run_script_from_command("SET_HEATER_TEMPERATURE HEATER=%s TARGET=%d" % (heater_name,load_temp))
@@ -136,6 +119,7 @@ class FILPAL:
             cmd_LOAD_CONTINUE_help = "contiunues the filament load of FILPAL_SWAP"
             def cmd_LOAD_CONTINUE(self,gcmd):
                 self.gcode.run_script_from_command("G1 F100 E500")
+                #add code to update the current loaded fillament in fil.pal file
 
 
     def cmd_FILPAL_CONVERTER(self, gcmd): #this command is used by filpal to parse through an existing gcode file and pull all of the numerical values associated with specfifc gcode commands. It then adds a line at the start of the gcode with a dictionary of these values
@@ -147,45 +131,60 @@ class FILPAL:
 
     def cmd_FILPAL_UPDATER(self, gcmd):  #this command is used to update the parameter values in fil.pal for the loaded filament. Also can create new parameters if in the accepted list
         fila ={}
+        fil_id=gcmd.get("FILA_ID",False)
         allowed_params=["hotend_min_temp","hotend_max_temp","hotend_initial_temp","hotend_temp","retraction","fan_min","fan_max","z_offset","filament_type","flowrate"]
-        params=None
-        param=None
-        try:
-            params_raw=gcmd.get("PARAMS")
-            params=ast.literal_eval(params)
-        except:
-            param=gcmd.get("PARAM")
-            param_val=gcmd.getfloat("VALUE")
+        if gcmd.get("lookup") != True:    
+            param=gcmd.get("PARAM",False)
+            param_val=gcmd.getfloat("VALUE",False)
+            params_raw=gcmd.get("PARAMS",False)
+            params=ast.literal_eval(params_raw)
         varfile = configparser.ConfigParser()
         try:
+            f=open(self.params_file)
             varfile.read(self.params_file)
-            if varfile.has_section('Loaded'):
-                if varfile.has_section(varfile["Loaded"]["fila_ID"]):
-                    for name, val in varfile.items('Variables'):
-                        fila[name] = ast.literal_eval(val)  
+            f.close()
+            if varfile.has_section('loaded'):
+                if fil_id == False:
+                    id=ast.literal_eval(varfile["loaded"]["fila_id"])
+                else:
+                    id=ast.literal_eval(fil_id)
+                if varfile.has_section(id):
+                    for name, val in varfile.items(id):
+                        fila[name]=ast.literal_eval(val)  
+                else:
+                    msg = "Unable to parse Loaded Filament paramters. Please run FILPAL_LOAD or update the parameters file"
+                    logging.exception(msg)
+                    raise self.printer.command_error(msg)
         except:
             msg = "Unable to parse Loaded Filament paramters. Please run FILPAL_LOAD or update the parameters file"
             logging.exception(msg)
             raise self.printer.command_error(msg)
+
+        if gcmd =="lookup":
+            return fila
+
         if params:
             for paras in params:
                 if paras in fila:
-                    fila[paras]=params[paras]
+                    varfile.set(id,paras,str(params[paras]))
                 elif paras in allowed_params:
-                    fila[paras]=params[paras]
+                    varfile.set(id,paras,str(params[paras]))
                 else:
                     msg = "The parameter %s is not recognized. Please check spelling" % (paras)
                     logging.exception(msg)
                     raise self.printer.command_error(msg)
         if param:
             if param in fila:
-                fila[param]=param_val
+                varfile.set(id,param,str(param_val))  #this isnt the correct way to update the file. need to write to actual file
             elif param in allowed_params:
-                fila[param]=param_val
+                varfile.set(id,param,str(param_val))
             else:
                 msg = "The parameter %s is not recognized. Please check spelling" % (param)
                 logging.exception(msg)
                 raise self.printer.command_error(msg)            
-            
+        f=open(self.params_file,"w")
+        varfile.write(f)
+        f.close()
+
 def load_config(config):
     return FILPAL(config)
